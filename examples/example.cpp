f965bc6233ed0d70f637bd0958c4b0475ebdb16e
noahfox555@gmail.com <noahfox555@gmail.com>
2022-11-11 14:42:41 -0500

    compile comment

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,5 +1,7 @@
 #include <pybind11/pybind11.h>
 
+// To compile c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
+
 int add(int i, int j) {
     return i + j;
 }
