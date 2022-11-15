6b8850d866df8fcb4a4f19b4df0ed31785062b19
jgriel <jgriel1@fandm.edu>
2022-11-15 13:32:55 -0500

    added function for subtraction of two vectors

--- examples/example.cpp
+++ examples/example.cpp
@@ -20,9 +20,22 @@ std::vector<int> addVectorVector(std::vector<int> arr_x, std::vector<int> arr_y)
     return arr_z;
 }
 
+std::vector<int> subtractVectorVector(std::vector<int> arr_x, std::vector<int> arr_y) {
+    if (arr_x.size() != arr_y.size()){
+        throw std::invalid_argument("Length of vectors must the same!");
+    }
+
+    std::vector<int> arr_z = {};
+    for (size_t i = 0; i < arr_x.size(); i++) {
+        arr_z.push_back(arr_x[i] - arr_y[i]);
+    }
+    return arr_z;
+}
+
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
     m.def("addVectorVector", &addVectorVector, "A function that adds together values at equivalent indices between two vectors");
+    m.def("subtractVectorVector", &subtractVectorVector, "A function that subtracts two values at equivalent indices between two vectors");
 }
