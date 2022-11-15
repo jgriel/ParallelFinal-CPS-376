3fdded3f171b06932bc827cca60201240f5c9136
jgriel <jgriel1@fandm.edu>
2022-11-15 13:25:59 -0500

    added function for vector addition between two vectors, including working error checking if vectors are not the same size

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,4 +1,6 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <vector>
 
 // To compile c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
 
@@ -6,8 +8,21 @@ int add(int i, int j) {
     return i + j;
 }
 
+std::vector<int> addVectorVector(std::vector<int> arr_x, std::vector<int> arr_y) {
+    if (arr_x.size() != arr_y.size()){
+        throw std::invalid_argument("Length of vectors must the same!");
+    }
+
+    std::vector<int> arr_z = {};
+    for (size_t i = 0; i < arr_x.size(); i++) {
+        arr_z.push_back(arr_x[i] + arr_y[i]);
+    }
+    return arr_z;
+}
+
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
+    m.def("addVectorVector", &addVectorVector, "A function that adds together values at equivalent indices between two vectors");
 }
--- examples/pythonExample.py
+++ examples/pythonExample.py
@@ -2,3 +2,4 @@ import example
 
 print(example.add(1, 2))
 print(example.add(5, 2))
+print(example.addVectorVector([1,2,3], [2,4,6]))
