230dc67d5a928441f07a4aa34cb41f5656049c75
jgriel <jgriel1@fandm.edu>
2022-11-15 12:26:49 -0500

    added function to do scalar multiplication between and int and a vector of ints

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,4 +1,6 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <vector>
 
 // To compile c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
 
@@ -6,8 +8,16 @@ int add(int i, int j) {
     return i + j;
 }
 
+std::vector<int> scalarVector(int x, std::vector<int> arr) {
+    for (size_t i = 0; i < arr.size(); i++) {
+        arr[i] *= x;
+    }
+    return arr;
+}
+
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
+    m.def("scalarVector", &scalarVector, "A function that takes an integer, and performs scalar multiplication on a vector");
 }
