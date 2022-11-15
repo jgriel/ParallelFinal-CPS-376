7738020faf79f49e068d6e6a483f3f1451dc21e7
jgriel <84743102+jgriel@users.noreply.github.com>
2022-11-15 12:34:02 -0500

    remove accidentally included the include statement from the scalar branch
    fixes inevitable merge conflict.

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,6 +1,4 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <vector>
 
 // To compile c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
 
