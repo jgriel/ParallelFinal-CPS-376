4d46f16bc6e59becaee00fadd77fecb62039ab91
jgriel <jgriel1@fandm.edu>
2022-11-15 14:10:23 -0500

    added function that does vector-matrix multiplication, with error checking for length of each row!

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,4 +1,6 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <vector>
 
 // To compile c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
 
@@ -6,8 +8,24 @@ int add(int i, int j) {
     return i + j;
 }
 
+std::vector<std::vector<int>> multiplyVectorMatrix(std::vector<int> arr, std::vector<std::vector<int>> mat) {
+    std::vector<std::vector<int>> mat_b = {};
+    for (size_t i = 0; i < mat.size(); i++) {
+        if (arr.size() != mat[i].size()) {
+            throw std::invalid_argument("Length of all rows in the matrix must == legnth of the vector!");
+        }
+
+        mat_b.push_back({});
+        for (size_t j = 0; j < mat[i].size(); j++) {
+            mat_b[i].push_back(arr[j] * mat[i][j]);
+        }
+    }
+    return mat_b;
+}
+
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
+    m.def("multiplyVectorMatrix", &multiplyVectorMatrix, "A function that does vector-matrix multiplication");
 }
--- examples/pythonExample.py
+++ examples/pythonExample.py
@@ -2,3 +2,4 @@ import example
 
 print(example.add(1, 2))
 print(example.add(5, 2))
+print(example.multiplyVectorMatrix([1,2,3], [[1,2,3], [4,5,6]]))
