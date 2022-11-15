8c87781597764829d673c27cfc6de06809cb5e2e
jgriel <jgriel1@fandm.edu>
2022-11-15 13:00:23 -0500

    added function to perform scalar multiplication between integer and matrix of integers

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,4 +1,6 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <vector>
 
 // To compile c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
 
@@ -6,8 +8,18 @@ int add(int i, int j) {
     return i + j;
 }
 
+std::vector<std::vector<int>> scalarMatrix(int x, std::vector<std::vector<int>> mat) {
+    for (size_t i = 0; i < mat.size(); i++) {
+        for (size_t j = 0; j < mat[i].size(); j++) {
+            mat[i][j] *= x;
+        }
+    }
+    return mat;
+}
+
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
+    m.def("scalarMatrix", &scalarMatrix, "A function that performs scalar multiplication between an integer and a matrix of integers");
 }
--- examples/pythonExample.py
+++ examples/pythonExample.py
@@ -2,3 +2,4 @@ import example
 
 print(example.add(1, 2))
 print(example.add(5, 2))
+print(example.scalarMatrix(2, [[1,2,3], [4,5,6]]))
