cbe737fcef36cb5d3be6cef58b0bf143b00e2e1a
jgriel <jgriel1@fandm.edu>
2022-11-15 12:14:22 -0500

    added function to subtract two integers and function to multiply two integers

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,4 +1,6 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <vector>
 
 // To compile c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
 
@@ -6,8 +8,18 @@ int add(int i, int j) {
     return i + j;
 }
 
+int subtractInt(int i, int j) {
+    return i - j;
+}
+
+int multiplyInt(int i, int j) {
+    return i * j;
+}
+
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
+    m.def("subtract", &subtractInt, "A function that subtracts two numbers");
+    m.def("multiply", &multiplyInt, "A function that multiplies two numbers");
 }
