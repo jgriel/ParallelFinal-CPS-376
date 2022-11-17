1f2d634228e27b340487d8bf9c1a6b364f184f8a
jgriel <jgriel1@fandm.edu>
2022-11-17 12:52:21 -0500

    added function for matrix multiplication

--- examples/example.cpp
+++ examples/example.cpp
@@ -4,8 +4,35 @@
 
 // To compile c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
 
-int add(int i, int j) {
+int addInt(int i, int j) {
     return i + j;
+}
+
+int subtractInt(int i, int j) {
+    return i - j;
+}
+
+int multiplyInt(int i, int j) {
+    return i * j;
+}
+
+std::vector<std::vector<int>> multiplyMatrixMatrix(std::vector<std::vector<int>> m1, std::vector<std::vector<int>> m2) {
+    std::vector<std::vector<int>> m3 = {};
+    if (m1[0].size() != m2.size()) {
+        throw std::invalid_argument("Length of rows in matrix #1 must equal length of columns in matrix #2!");
+    }
+    for (size_t m1_row = 0; m1_row < m1.size(); m1_row++) {
+        std::vector<int> tmp = {};
+        for (size_t m2_col = 0; m2_col < m2[0].size(); m2_col++) {
+            int acc = 0;
+            for (size_t idx = 0; idx < m1[m1_row].size(); idx++) {
+                acc += m1[m1_row][idx] * m2[idx][m2_col];
+            }
+            tmp.push_back(acc);
+        }
+        m3.push_back(tmp);
+    }
+    return m3;
 }
 
 std::vector<std::vector<int>> multiplyVectorMatrix(std::vector<int> arr, std::vector<std::vector<int>> mat) {
@@ -75,23 +102,16 @@ std::vector<int> scalarVector(int x, std::vector<int> arr) {
     return arr;
 }
 
-int subtractInt(int i, int j) {
-    return i - j;
-}
-
-int multiplyInt(int i, int j) {
-    return i * j;
-}
-
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
-    m.def("add", &add, "A function that adds two numbers");
+    m.def("addInt", &addInt, "A function that adds two numbers");
+    m.def("subtractInt", &subtractInt, "A function that subtracts two numbers");
+    m.def("multiplyInt", &multiplyInt, "A function that multiplies two numbers");
     m.def("multiplyVectorMatrix", &multiplyVectorMatrix, "A function that does vector-matrix multiplication");
     m.def("addVectorVector", &addVectorVector, "A function that adds together values at equivalent indices between two vectors");
     m.def("subtractVectorVector", &subtractVectorVector, "A function that subtracts two values at equivalent indices between two vectors");
     m.def("multiplyVectorVector", &multiplyVectorVector, "A function that multiplies two values at equivalent indices between two vectors");
     m.def("scalarMatrix", &scalarMatrix, "A function that performs scalar multiplication between an integer and a matrix of integers");
     m.def("scalarVector", &scalarVector, "A function that takes an integer, and performs scalar multiplication on a vector");
-    m.def("subtract", &subtractInt, "A function that subtracts two numbers");
-    m.def("multiply", &multiplyInt, "A function that multiplies two numbers");
+    m.def("multiplyMatrixMatrix", &multiplyMatrixMatrix, "A function that peforms matrix multiplication on two matrices of integers");
 }
--- examples/pythonExample.py
+++ examples/pythonExample.py
@@ -1,10 +1,11 @@
 import example
 
-print(example.add(1, 2))
-print(example.add(5, 2))
+print(example.addInt(1, 2))
+print(example.addInt(5, 2))
+print(example.subtractInt(79, 10))
+print(example.multiplyInt(12, 5))
 
 print(example.multiplyVectorMatrix([1,2,3], [[1,2,3], [4,5,6]]))
-
 
 print(example.addVectorVector([1,2,3], [2,4,6]))
 print(example.subtractVectorVector([2,4,6], [1,2,3]))
@@ -13,5 +14,7 @@ print(example.multiplyVectorVector([2,4,6], [2,4,6]))
 print(example.scalarMatrix(2, [[1,2,3], [4,5,6]]))
 
 print(example.scalarVector(2, [1,2,3,4,5]))
-print(example.subtract(79, 10))
-print(example.multiply(12, 5))
+
+print(example.multiplyMatrixMatrix( [[6,5], [4,3], [2,1]], [[1,2,3], [4,5,6]] ))
+
+print
