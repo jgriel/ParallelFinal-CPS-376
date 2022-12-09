1379fb814da47ea18345facb457d77e02a9065b3
noahfox555@gmail.com <noahfox555@gmail.com>
2022-12-09 15:37:31 -0500

    python still crashes on import but now using cmake

---
+++ WorseNumPy/CMakeFiles/WorseNumPy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WorseNumPy.dir/src/WorseNumPy.cpp.o"
+  "WorseNumPy.cpython-38-x86_64-linux-gnu.so"
+  "WorseNumPy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WorseNumPy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
